5d9a81a9bb9cc0966d6fb79f8d6f7385c93008eb
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-23 22:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const element = document.getElementById('dodger')
 document.addEventListener('keydown', (event) => {
-  
+  console.log(event) //this is just to see what is happening
+  if(event.which === 37) {
+    event.style.backgroundColor = 'yellow'
+  }
 })
