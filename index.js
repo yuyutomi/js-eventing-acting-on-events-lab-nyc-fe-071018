2ca4a894dc2929b371df5be91cd112215a785ee7
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-23 22:30:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const element = document.getElementById('dodger')
 document.addEventListener('keydown', (event) => {
   console.log(event) //this is just to see what is happening
-  if((event.which === 37) && (element.style.left > 0)) {
+  if(event.which === 37 && element.style.left > 0) {
     // 180px
     let previousLeft = element.style.left
     // 180
