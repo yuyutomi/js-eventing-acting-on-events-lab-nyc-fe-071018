04726f49a8811ce0a239842471a175dfc530eced
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-23 22:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ document.addEventListener('keydown', (event) => {
 })
 
 // To pass this test, we'll use the following, but the above method is ideal.
+ function 
+  if(event.which === 39) {
+    // we can't do element.style.right because we're not given a 'right' styling
+    let previousRight = element.style.left 
+    // 180
+    let previousRightNumber = parseInt(previousRight.replace('px', ''), 10)
+    if(previousRightNumber < 400) {
+      let newRightNumber = (previousRightNumber + 10) + 'px'
+      element.style.right = newRightNumber
+    }
+  }
