7fc74e75b00d5652ba81c2b555a06aa7cfdf1b6b
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-23 22:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const element = document.getElementById('dodger')
 document.addEventListener('keydown', (event) => {
   console.log(event) //this is just to see what is happening
   if(event.which === 37) {
+    // 180px
     let previousLeft = element.style.left
+    // 180
+    let previousLeftNumber = parseInt(previousLeft.replace('px', ''))
   }
 })
