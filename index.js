3cee6aab8c93c21c2e27d30c4484834a544244c1
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-23 22:24:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ document.addEventListener('keydown', (event) => {
     let previousLeft = element.style.left
     // 180
     let previousLeftNumber = parseInt(previousLeft.replace('px', ''), 10)
-    let newLeftNumber = previousLeftNumber - 1 
+    let newLeftNumber = (previousLeftNumber - 1) + 'px'
   }
 })
