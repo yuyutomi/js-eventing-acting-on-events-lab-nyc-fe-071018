f3692e5ef05da9a8b4c8ed46927bc8e111a16e86
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-23 22:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ document.addEventListener('keydown', (event) => {
     let previousRight = element.style.left 
     // 180
     let previousRightNumber = parseInt(previousRight.replace('px', ''), 10)
-    if(previousRightNumber > 0) {
+    if(previousRightNumber < 400) {
       let newRightNumber = (previousRightNumber + 10) + 'px'
       element.style.right = newRightNumber
     }
